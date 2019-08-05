22add30f8146064231cdb715118710594e3646fb
baglansariev <baglansariev@mail.ru>
2019-08-05 19:39:41 +0600

    font-awesome has been added

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -7,6 +7,7 @@
 	<meta name="description" content="<?= $this->asset->getMetaDesc() ?>">
 	<meta name="keywords" content="<?= $this->asset->getMetaKeys() ?>">
 	<title><?= $this->asset->getTitle() ?></title>
+    <link  rel="stylesheet" href="/public/style/font-awesome/css/all.min.css">
 	<link rel="shortcut icon" href="/public/images/icons/favicon.png" type="image/x-icon">
 	<link rel="stylesheet" href="/public/style/css/main.css">
 	<link rel="stylesheet" href="/public/style/bootstrap/bootstrap-grid.css">
@@ -15,6 +16,7 @@
 	<link rel="stylesheet" href="<?= $css ?>">
 	<? endforeach ?>
 	<script type="text/javascript" src="/public/style/js/jquery-3.4.0.min.js"></script>
+    <script type="text/javascript" src="/public/style/font-awesome/js/all.min.js"></script>
 	<script type="text/javascript" src="/public/style/js/app.js"></script>
 	<? foreach($this->asset->getJs() as $js): ?>
 	<script type="text/javascript" src="<?= $js ?>"></script>
